51b4e00554ef3d49cd50749718954e6a6920da42
Jerry Wang <69374275+JDxWang@users.noreply.github.com>
2021-02-03 15:38:50 +1100

    Hover issue fix

--- app.R
+++ app.R
@@ -7,26 +7,26 @@ ui <- fluidPage(
   fluidRow(
     column(12,
            plotOutput(outputId = "tag_freq",
-             hover = hoverOpts("hover")),
-             div(style = "height:420px")
-          ),
-         ),
+                      hover = hoverOpts("hover")),
+           div(style = "height:420px")
+    ),
+  ),
   fluidRow(
     column(6,
            h3("This app shows the frequency of tags for the newest X pages of Ozbargain posts."),
            numericInput("pages", label = "Choose the number of pages to load (1 to 10)", value = 10, max = 10, min = 1)
-           ),
+    ),
     column(6,
            h3("Hover over any column to see the top deal of that category!"),
            h4(uiOutput("top_deals"))
-          )
-         )
+    )
   )
+)
 
 server <- function(input, output) {
   ozb <- "https://www.ozbargain.com.au/deals?page="
   ozb_data <- data.frame()
-
+  
   for(i in c(0:9)){
     page <- read_html(paste(ozb, i, sep=""))
     
@@ -48,25 +48,27 @@ server <- function(input, output) {
     head(ozb_data$tags, 30*input$pages) %>% table() %>% as.data.frame() %>% arrange(Freq)
   })
   
-  output_topdeals <- ozb_data %>% group_by(tags) %>% slice(which.max(upvote))
-
+  output_topdeals <- reactive({
+    ozb_data %>% head(30*input$pages) %>% group_by(tags) %>% slice(which.max(upvote))
+  })
+  
   output$top_deals <- renderText({
     if(is.null(input$hover$x)) return()
     else {
-      x_tags <- round(input$hover$x) == output_topdeals$tags %>% as.factor() %>% as.numeric
-      output_topdeals$title[x_tags]
+      hover_tag <- output_tags()$.[round(input$hover$x)]
+      output_topdeals()$title[output_topdeals()$tags == hover_tag]
     }
   })
   
   output$tag_freq <- renderPlot({
     output_tags() %>% ggplot(aes(x=factor(output_tags()$., level=output_tags()$.), y=Freq, fill=.)) +
-                               labs(title="Frequency of Tags", x="Tag", y="Frequency") +
-                               geom_bar(stat="identity") +
-                               geom_text(aes(label=Freq), vjust = 1.1, color = "black", size = 7) +
-                               theme(legend.position = "none", text=element_text(size=20), axis.text.x = element_text(angle=45, hjust=1))
-    },
-    height = 800
-    )
-  }
+      labs(title="Frequency of Tags", x="Tag", y="Frequency") +
+      geom_bar(stat="identity") +
+      geom_text(aes(label=Freq), vjust = 1.1, color = "black", size = 7) +
+      theme(legend.position = "none", text=element_text(size=20), axis.text.x = element_text(angle=45, hjust=1))
+  },
+  height = 800
+  )
+}
 
 shinyApp(ui = ui, server = server)
